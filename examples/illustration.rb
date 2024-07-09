c2d2cddcfd458426e4aaf62b66d5b556a0c2763a
Mike Blumtritt <mike.blumtritt@pm.me>
2024-07-09 10:04:34 +0200

    Tweak illustration

--- examples/illustration.rb
+++ examples/illustration.rb
@@ -7,10 +7,9 @@ ui.framed do
     This is the [[c4]]beautiful[[/]], [[c5]]nice[[/]], [[c6]]nifty[[/]],
     [[c7]]fancy[[/]], [[c8]]neat[[/]], [[c9]]pretty[[/]],  [[ca]]cool[[/]],
     [[cb]]lovely[[/]], [[cc]]natty[[/]] [[bold]]user interface[[/]] you like to
-    have for your command line applications. It contains
-    [[curly_underline ul_c4]]elegant[[/]], [[italic]]simple[[/]] and
-    [[double_underline ul_cc]]beautiful[[/]] tools that enhance your command
-    line interfaces functionally and aesthetically.
+    have for your command line applications. It contains [[italic]]elegant[[/]],
+    [[italic]]simple[[/]] and [[italic]]beautiful[[/]] tools that enhance your
+    command line interfaces functionally and aesthetically.
   TEXT
 
   ui.msg('Features', glyph: '⭐️') { ui.ls(<<~FEATURES.lines(chomp: true)) }
