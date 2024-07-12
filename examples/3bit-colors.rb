97570936f12148674f3e4b5b715057725bc855f7
Mike Blumtritt <mike.blumtritt@pm.me>
2024-07-12 18:00:29 +0200

    Tidy-up 3bit.colors example

--- examples/3bit-colors.rb
+++ examples/3bit-colors.rb
@@ -6,9 +6,9 @@ ui.space
 ui.h1 'NattyUI: 3/4-bit Color Support'
 ui.space
 
-color = ->(n) { "[[#{n}]]#{n.ljust(14)}[[/]] [[on_#{n}]] sample text [[/]]" }
+color = ->(n, j) { "[[#{n}]]#{n.ljust(j)}[[/]] [[on_#{n}]] sample text [[/]]" }
 %w[black red green yellow blue magenta cyan white].each do |name|
-  ui.puts "#{color[name]}    #{color["bright_#{name}"]}"
+  ui.puts "#{color[name, 7]}    #{color["bright_#{name}", 14]}"
 end
 
 ui.space
