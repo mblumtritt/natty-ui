b71a7c1057512a48b1999de432554f287d23d9b6
mblumtritt <mblumtritt@tutamail.com>
2025-08-10 15:56:04 +0200

    Optimize `info` example

--- examples/info.rb
+++ examples/info.rb
@@ -2,28 +2,29 @@
 
 require_relative '../lib/natty-ui'
 
+def colors
+  return Terminal.colors.to_s if Terminal.colors < 8
+  colors = [
+    Terminal.true_color? ? 'true color' : Terminal.colors,
+    "#{(0..15).map { "[#{_1.to_s(16).rjust(2, '0')}]██" }.join}[/]"
+  ]
+  if Terminal.true_color?
+    colors << Terminal::Ansi.rainbow('████████████████████████████████')
+  end
+  colors.join("\n")
+end
+
 ui.message '[b]​ᓚᕠᗢ NattyUI[/b] [i green]Terminal Information[/]' do
   ui.space
   ui.table do |table|
     table.add('Identifier', Terminal.application || 'unidentified')
     table.add('ANSI support', Terminal.ansi? ? 'yes' : 'no')
     table.add('Input mode', Terminal.input_mode)
-    table.add(
-      'Colors',
-      "#{Terminal.true_color? ? 'true color' : Terminal.colors}
-      [black]██[red]██[green]██[yellow]██[blue]██[magenta]██[cyan]██[white]██" \
-        '[bright_black]██[bright_red]██[bright_green]██[bright_yellow]██' \
-        '[bright_blue]██[bright_magenta]██[bright_cyan]██[bright_white]██[/]' \
-        "#{
-          if Terminal.true_color?
-            Terminal::Ansi.rainbow("\n████████████████████████████████")
-          end
-        }"
-    )
+    table.add('Colors', colors)
     table.add('Screen size', Terminal.size.join(' x '))
     fc = table.columns[0]
-    fc.width = 13
-    fc.padding_right = 1
+    fc.width = 14
+    fc.padding_right = 2
     fc.align = :right
     table.columns[1].style = :bold
   end
