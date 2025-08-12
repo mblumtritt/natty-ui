ddb4e26525aa644f93ecb19992f257137d21aaac
mblumtritt <mblumtritt@tutamail.com>
2025-08-12 09:44:52 +0200

    Refactor bars again

--- lib/natty-ui/hbars_renderer.rb
+++ lib/natty-ui/hbars_renderer.rb
@@ -5,39 +5,51 @@ require_relative 'utils'
 module NattyUI
   module HBarsRenderer
     class << self
-      def lines(values, max_size, normalize, style, text_style)
-        texts = values.map { Str.new(_1) }
-        tw = texts.max_by(&:width).width
-        size = max_size - tw - 1
-        return if size < 2 # TODO may draw only text
-        values = normalize ? normalize!(values, size) : adjust!(values, size)
-        if style
-          bos = Ansi[*style]
-          eos = Ansi::RESET
-        end
+      def lines(vals, width, normalize, style, text_style)
         if text_style
           bots = Ansi[*text_style]
           eots = Ansi::RESET
         end
+        texts = vals.map { Str.new(_1) }
+        tw = texts.max_by(&:width).width
+        size = width - tw - 1
+        if size < 2 # text only
+          return texts.map { "#{bots}#{' ' * (tw - _1.width)}#{_1}#{eots}" }
+        end
+        if style
+          bos = Ansi[*style]
+          eos = Ansi::RESET
+        end
+        vals = normalize ? normalize!(vals, size) : adjust!(vals, size)
         texts.map.with_index do |str, idx|
           "#{bots}#{' ' * (tw - str.width)}#{str}#{eots}#{bos}▕#{
-            '▆' * values[idx]
+            '▆' * vals[idx]
           }#{eos}"
         end
       end
 
-      private
-
-      def adjust!(values, size)
-        max = values.max.to_f
-        values.map { ((_1 / max) * size).round }
+      def lines_bars_only(vals, width, normalize, style)
+        if style
+          bos = Ansi[*style]
+          eos = Ansi::RESET
+        end
+        width -= 1
+        vals = normalize ? normalize!(vals, width) : adjust!(vals, width)
+        vals.map { "#{bos}▕#{'▆' * _1}#{eos}" }
       end
 
-      def normalize!(values, size)
-        min, max = values.minmax
-        return Array.new(values.size, size) if min == max
+      private
+
+      def adjust!(vals, size)
+        max = vals.max.to_f
+        vals.map { ((_1 / max) * size).round }
+      end
+
+      def normalize!(vals, size)
+        min, max = vals.minmax
+        return Array.new(vals.size, size) if min == max
         max = (max - min).to_f
-        values.map { (((_1 - min) / max) * size).round }
+        vals.map { (((_1 - min) / max) * size).round }
       end
     end
   end
--- lib/natty-ui/vbars_renderer.rb
+++ lib/natty-ui/vbars_renderer.rb
@@ -3,45 +3,43 @@
 module NattyUI
   module VBarsRenderer
     class << self
-      def lines(values, max_size, height, normalize, bar_width, style)
-        size = height - 1
+      def lines(vals, width, height, normalize, bar_width, style)
         if style
           bos = Ansi[*style]
           eos = Ansi::RESET
         end
-        if (bar_width == :min) || (values.size * 2 > max_size)
+        if ((bar_width != :auto) && bar_width <= 1) || (vals.size * 2 > width)
           db = '▉'
           ds = ' '
-          el = '▔' * values.size
-          values = values.take(max_size) if values.size > max_size
+          el = "#{bos}#{'▔' * vals.size}#{eos}"
+          vals = vals.take(width) if vals.size > width
         else
-          bw = (max_size - values.size) / values.size
+          bw = (width - vals.size) / vals.size
           bw = [bw, bar_width].min if bar_width != :auto
           db = "#{'█' * bw} "
           bw += 1
           ds = ' ' * bw
-          el = '▔' * ((bw * values.size) - 1)
+          el = "#{bos}#{'▔' * ((bw * vals.size) - 1)}#{eos}"
         end
-        values = normalize ? normalize!(values, size) : adjust!(values, size)
-        size
+        height -= 1
+        vals = normalize ? normalize!(vals, height) : adjust!(vals, height)
+        height
           .downto(1)
-          .map do |i|
-            "#{bos}#{values.map { _1 < i ? ds : db }.join}#{eos}"
-          end << "#{bos}#{el}#{eos}"
+          .map { |i| "#{bos}#{vals.map { _1 < i ? ds : db }.join}#{eos}" } << el
       end
 
       private
 
-      def adjust!(values, size)
-        max = values.max.to_f
-        values.map { ((_1 / max) * size).round }
+      def adjust!(vals, height)
+        max = vals.max.to_f
+        vals.map { ((_1 / max) * height).round }
       end
 
-      def normalize!(values, size)
-        min, max = values.minmax
-        return Array.new(values.size, size) if min == max
+      def normalize!(vals, height)
+        min, max = vals.minmax
+        return Array.new(vals.size, height) if min == max
         max = (max - min).to_f
-        values.map { (((_1 - min) / max) * size).round }
+        vals.map { (((_1 - min) / max) * height).round }
       end
     end
   end
